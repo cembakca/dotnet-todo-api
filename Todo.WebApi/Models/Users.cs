07f03f3ba6c940ba7743fa8afd6551152041635a
Cem Bakca <cem.bakca@dygdigital.com>
2020-01-30 17:48:06 +0300

    fixed auth controller

--- Todo.WebApi/Models/Users.cs
+++ Todo.WebApi/Models/Users.cs
@@ -1,11 +1,17 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Todo.WebApi.Models
 {
     public class Users : BaseEntity
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
         public string Password { get; set; }
+        
+        [Required(ErrorMessage = "Email alanı zorunludur.")]
+        [EmailAddress(ErrorMessage = "Lütfen geçerli mail adresi giriniz.")]
         public string Email { get; set; }
 
         public virtual List<Todo> Todos { get; set; }
